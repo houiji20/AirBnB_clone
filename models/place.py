ae737a7c545af6c67502336ab8eee85faf816dbc
houiji20 <91070531+houiji20@users.noreply.github.com>
2022-03-06 19:55:01 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-from .base_model import BaseModel
+"""
+Place module
+"""
+
+from models.base_model import BaseModel
+
 
 class Place(BaseModel):
+    """
+    Place class
+    """
     city_id = ""
     user_id = ""
     name = ""
@@ -12,4 +20,4 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = [] 
+    amenity_ids = []
