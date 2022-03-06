14cdc0c67f723d0a823975017c69fb2f8ac50eba
houiji20 <91070531+houiji20@users.noreply.github.com>
2022-03-06 19:55:59 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-from .base_model import BaseModel
+"""
+state module
+"""
+
+from models.base_model import BaseModel
+
 
 class State(BaseModel):
+    """
+    state class
+    """
     name = ""
