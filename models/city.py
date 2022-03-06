f30d9d7ab70fa2991ad8e26ec343ae767ce88adc
houiji20 <91070531+houiji20@users.noreply.github.com>
2022-03-06 19:47:52 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-from .base_model import BaseModel
+"""
+City module
+"""
+
+from models.base_model import BaseModel
+
 
 class City(BaseModel):
+    """
+    City class
+    """
     state_id = ""
     name = ""
